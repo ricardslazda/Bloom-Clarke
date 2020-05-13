6bf71e31934035d64bbc5d47eed2e91a1fd70615
unknown <ricardslazda28@gmail.com>
2020-05-13 12:50:40 +0300

    bug fix

--- resources/assets/js/loader.js
+++ resources/assets/js/loader.js
@@ -9,7 +9,6 @@
                 loader.css('z-index', -1);
                 loader.css('opacity', 0);
                 property.css('opacity', 1);
-                property.css('z-index', -10);
                 $('body').removeClass('stop-scroll');
                 $('html').removeClass('stop-scroll');
             }, 1000);
